3c716b4e6a12d5d7cf090c2e9ba63ebbcf88f72c
ShoumoPal <arka.pal21@gmail.com>
2023-12-05 13:57:22 +0530

    Applied OOPs + Smart pointers

--- Main.cpp
+++ Main.cpp
@@ -12,276 +12,325 @@ const int map_size_y{ 30 };
 const int window_size_x{ 720 };
 const int window_size_y{ 720 };
 
-const int square_size = (int)(std::min(window_size_x / map_size_x, window_size_y / map_size_y));
-
 int map[map_size_y][map_size_x];
 int map_view[map_size_y][map_size_x];
-sf::RenderWindow app(sf::VideoMode(window_size_x, window_size_y), "Minesweeper!", sf::Style::Close);
 
-sf::Texture mine;
-sf::Texture flag;
-sf::Texture square;
-sf::Font font;
+int square_size = (int)(std::min(window_size_x / map_size_x, window_size_y / map_size_y));
 
-// Stores all the directions from one cell
+const int ways[8][2] = { { -1, -1 },
+		{ -1, 0 },
+		{ -1, 1 },
+		{ 0, -1 },
+		{ 0, 1 },
+		{ 1, -1 },
+		{ 1, 0 },
+		{ 1, 1 } };
 
-int ways[][2] = { { -1, -1 },
-	{ -1, 0 },
-	{ -1, 1 },
-	{ 0, -1 },
-	{ 0, 1 },
-	{ 1, -1 },
-	{ 1, 0 },
-	{ 1, 1 } };
+class Map {
+private:
 
-void ShowMine(int x, int y) {
-	sf::Sprite sprite;
-	sprite.setTexture(mine);
-	sprite.scale(sf::Vector2f((float)square_size / (float)mine.getSize().x, (float)square_size / (float)mine.getSize().y));
-	sprite.setPosition(sf::Vector2f(x * square_size, y * square_size));
-	app.draw(sprite);
-}
+	sf::RenderWindow app;
+	sf::Texture mine;
+	sf::Texture flag;
+	sf::Texture square;
+	sf::Font font;
 
-void ShowSquareSprite(int x, int y) {
-	sf::Sprite sprite;
-	sprite.setTexture(square);
-	sprite.scale(sf::Vector2f((float)square_size / (float)square.getSize().x, (float)square_size / (float)square.getSize().y));
-	sprite.setPosition(sf::Vector2f(x * square_size, y * square_size));
-	app.draw(sprite);
-}
+	// Stores all the directions from one cell
 
-void ShowColorText(int x, int y) {
-	sf::Color colors[] = {
-						sf::Color::Blue,
-						sf::Color::Green,
-						sf::Color::Red,
-						sf::Color::Magenta,
-						sf::Color::Cyan,
-						sf::Color::Blue,
-						sf::Color::Green,
-						sf::Color::Red,
-	};
+public:
+	// Constructor
 
-	sf::Text text;
-	text.setFont(font);
-	text.setStyle(sf::Text::Bold);
-	text.setString(std::to_string(map[y][x]));
-	text.setCharacterSize(square_size);
-	text.setFillColor(colors[map[y][x] - 1]);
-	text.setPosition(sf::Vector2f(x * square_size + square_size / 4, y * square_size - square_size / 6));
-	app.draw(text);
-}
+	Map() : app(sf::VideoMode(window_size_x, window_size_y), "Minesweeper!", sf::Style::Close) {
 
-// To set the squares on the Window
+		flag.loadFromFile("Textures/flag.png");
+		mine.loadFromFile("Textures/mine.png");
+		square.loadFromFile("Textures/Square.png");
 
-void SetSquareOnScreen(int x, int y) {
-	sf::RectangleShape rectangle(sf::Vector2f(x * square_size, y * square_size));
-	rectangle.setSize(sf::Vector2f(square_size, square_size));
-	rectangle.setPosition(sf::Vector2f(x * square_size, y * square_size));
-	rectangle.setFillColor(sf::Color::White);
-	rectangle.setOutlineColor(sf::Color::Black);
-	rectangle.setOutlineThickness(1);
-	app.draw(rectangle);
-}
-
-void ShowFlagONScreen(int x, int y) {
-	sf::Sprite sprite;
-	sprite.setTexture(flag);
-	sprite.scale(sf::Vector2f((float)square_size / (float)flag.getSize().x, (float)square_size / (float)flag.getSize().y));
-	sprite.setPosition(sf::Vector2f(x * square_size, y * square_size));
-	app.draw(sprite);
-}
-
-void GameOver(std::string t)
-{
-	sf::Text text;
-	text.setFont(font);
-	text.setString(t);
-	text.setFillColor(sf::Color::Red);
-	text.setCharacterSize(std::min(window_size_x, window_size_y) / 6);
-	app.draw(text);
-	app.display();
-	sf::Event e;
-	while (1)
-	{
-		while (app.pollEvent(e))
-		{
-			if (e.type == sf::Event::Closed)
-			{
-				app.close();
-				return;
-			}
-		}
+		font.loadFromFile("Textures/FutureLight-BW15w.ttf");
 	}
-}
 
-void CheckWin()
-{
-	for (int y = 0; y < map_size_y; y++)
-	{
-		for (int x = 0; x < map_size_x; x++)
-		{
-			if (!(map[y][x] == map_view[y][x] || (map[y][x] == -1 && map_view[y][x] == 9) || (map[y][x] == 0 && map_view[y][x] == 10)))
-			{
-				return;
-			}
-		}
+	sf::RenderWindow& GetWindow() {
+		return app;
 	}
-	std::string t = "You won!";
-	GameOver(t);
-	return;
-}
-void OpenAllZeroAround(int x, int y)
-{
-	for (auto i : ways)
-	{
 
-		if (x - i[0] >= 0 && x - i[0] < map_size_x && y - i[1] >= 0 && y - i[1] < map_size_y && (map_view[y - i[1]][x - i[0]] == 10))
-		{
-			map_view[y - i[1]][x - i[0]] = map[y - i[1]][x - i[0]];
-			if (map[y - i[1]][x - i[0]] == 0)
-			{
-				OpenAllZeroAround(x - i[0], y - i[1]);
-			}
-		}
+	sf::Font& GetFont() {
+		return font;
 	}
-}
 
-void SetNumbers()
-{
-	for (int y = 0; y < map_size_y; y++)
-	{
-		for (int x = 0; x < map_size_x; x++)
-		{
-			if (map[y][x] != -1)
-			{
-				int n = 0;
-				for (auto i : ways)
-				{
-					if (x - i[0] >= 0 && x - i[0] < map_size_x && y - i[1] >= 0 && y - i[1] < map_size_y && map[y - i[1]][x - i[0]] == -1)
-					{
-						n++;
-					}
-				}
-				map[y][x] = n;
-			}
-		}
+	sf::Texture GetMineTexture() {
+		return mine;
 	}
-}
 
-void GenerateMap()
-{
-	for (int y = 0; y < map_size_y; y++)
-	{
-		for (int x = 0; x < map_size_x; x++)
-		{
-			map_view[y][x] = 10;
-			if (random(0, 5) == 1)
-			{
-				map[y][x] = -1; //mine
-			}
-			else
-			{
-				map[y][x] = 0;
-			}
-		}
+	sf::Texture GetFlagTexture() {
+		return flag;
 	}
-	SetNumbers();
-}
 
-void Loop()
-{
-	sf::Event e;
-	while (app.isOpen())
+	sf::Texture GetSquareTexture() {
+		return square;
+	}
+
+	void ShowMine(int x, int y) {
+		sf::Sprite sprite;
+		sprite.setTexture(mine);
+		sprite.scale(sf::Vector2f((float)square_size / (float)mine.getSize().x, (float)square_size / (float)mine.getSize().y));
+		sprite.setPosition(sf::Vector2f(x * square_size, y * square_size));
+		app.draw(sprite);
+	}
+
+	void ShowSquareSprite(int x, int y) {
+		sf::Sprite sprite;
+		sprite.setTexture(square);
+		sprite.scale(sf::Vector2f((float)square_size / (float)square.getSize().x, (float)square_size / (float)square.getSize().y));
+		sprite.setPosition(sf::Vector2f(x * square_size, y * square_size));
+		app.draw(sprite);
+	}
+
+	void ShowColorText(int x, int y) {
+		sf::Color colors[] = {
+							sf::Color::Blue,
+							sf::Color::Green,
+							sf::Color::Red,
+							sf::Color::Magenta,
+							sf::Color::Cyan,
+							sf::Color::Blue,
+							sf::Color::Green,
+							sf::Color::Red,
+		};
+
+		sf::Text text;
+		text.setFont(font);
+		text.setStyle(sf::Text::Bold);
+		text.setString(std::to_string(map[y][x]));
+		text.setCharacterSize(square_size);
+		text.setFillColor(colors[map[y][x] - 1]);
+		text.setPosition(sf::Vector2f(x * square_size + square_size / 4, y * square_size - square_size / 6));
+		app.draw(text);
+	}
+
+	// To set the squares on the Window
+
+	void SetSquareOnScreen(int x, int y) {
+		sf::RectangleShape rectangle(sf::Vector2f(x * square_size, y * square_size));
+		rectangle.setSize(sf::Vector2f(square_size, square_size));
+		rectangle.setPosition(sf::Vector2f(x * square_size, y * square_size));
+		rectangle.setFillColor(sf::Color::White);
+		rectangle.setOutlineColor(sf::Color::Black);
+		rectangle.setOutlineThickness(1);
+		app.draw(rectangle);
+	}
+
+	void ShowFlagONScreen(int x, int y) {
+		sf::Sprite sprite;
+		sprite.setTexture(flag);
+		sprite.scale(sf::Vector2f((float)square_size / (float)flag.getSize().x, (float)square_size / (float)flag.getSize().y));
+		sprite.setPosition(sf::Vector2f(x * square_size, y * square_size));
+		app.draw(sprite);
+	}
+
+	void GenerateMap()
 	{
-		while (app.pollEvent(e))
-		{
-			if (e.type == sf::Event::Closed)
-			{
-				app.close();
-			}
-
-			if (e.type == sf::Event::MouseButtonPressed)
-			{
-
-				int x = (int)(e.mouseButton.x / square_size);
-				int y = (int)(e.mouseButton.y / square_size);
-
-				if (x < map_size_x && x >= 0 && y < map_size_y && y >= 0)
-				{
-					if (e.mouseButton.button == sf::Mouse::Right)
-					{
-						if (map_view[y][x] != 9 && map_view[y][x] == 10) // 9 - flag
-						{
-							map_view[y][x] = 9;
-						}
-						else if (map_view[y][x] == 9)
-						{
-							map_view[y][x] = 10;
-						}
-					}
-
-					if (e.mouseButton.button == sf::Mouse::Left)
-					{
-						map_view[y][x] = map[y][x];
-						if (map[y][x] == 0)
-						{
-							OpenAllZeroAround(x, y);
-						}
-						if (map[y][x] == -1)
-						{
-							std::string t = "Game over!";
-							ShowMine(x, y);
-							GameOver(t);
-							//return;
-						}
-						CheckWin();
-					}
-				}
-			}
-		}
-
-		//draw
-		app.clear(sf::Color::Black);
 		for (int y = 0; y < map_size_y; y++)
 		{
 			for (int x = 0; x < map_size_x; x++)
 			{
-				SetSquareOnScreen(x, y);
-				if (map_view[y][x] == 10) 
+				map_view[y][x] = 10;
+				if (random(0, 5) == 1)
 				{
-					ShowSquareSprite(x, y);
+					map[y][x] = -1; //mine
 				}
-				
-				if (map_view[y][x] == -1)
-				{ //mine
-					ShowMine(x, y);
-				}
-				else if (map_view[y][x] == 9)
+				else
 				{
-					ShowFlagONScreen(x, y);
-				}
-
-				else if (map_view[y][x] != 10)
-				{
-					ShowColorText(x, y);
+					map[y][x] = 0;
 				}
 			}
 		}
-		app.display();
+		SetNumbers();
 	}
-}
 
+	void OpenAllZeroAround(int x, int y)
+	{
+		for (auto i : ways)
+		{
+
+			if (x - i[0] >= 0 && x - i[0] < map_size_x && y - i[1] >= 0 && y - i[1] < map_size_y && (map_view[y - i[1]][x - i[0]] == 10))
+			{
+				map_view[y - i[1]][x - i[0]] = map[y - i[1]][x - i[0]];
+				if (map[y - i[1]][x - i[0]] == 0)
+				{
+					OpenAllZeroAround(x - i[0], y - i[1]);
+				}
+			}
+		}
+	}
+
+	void SetNumbers()
+	{
+		for (int y = 0; y < map_size_y; y++)
+		{
+			for (int x = 0; x < map_size_x; x++)
+			{
+				if (map[y][x] != -1)
+				{
+					int n = 0;
+					for (auto i : ways)
+					{
+						if (x - i[0] >= 0 && x - i[0] < map_size_x && y - i[1] >= 0 && y - i[1] < map_size_y && map[y - i[1]][x - i[0]] == -1)
+						{
+							n++;
+						}
+					}
+					map[y][x] = n;
+				}
+			}
+		}
+	}
+};
+
+class GameManager {
+private:
+	Map* mineMap{NULL};
+	bool isGameOver;
+public:
+	GameManager(Map *_mineMap) {
+		mineMap = _mineMap;
+		isGameOver = false;
+	}
+
+	void GameOver(std::string t)
+	{
+		isGameOver = true;
+		sf::Text text;
+		text.setFont(mineMap->GetFont());
+		text.setString(t);
+		text.setFillColor(sf::Color::Red);
+		text.setCharacterSize(std::min(window_size_x, window_size_y) / 6);
+		mineMap->GetWindow().draw(text);
+		mineMap->GetWindow().display();
+		sf::Event e;
+		std::cout << "Game Over: " << t << std::endl;
+		while (mineMap->GetWindow().pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+			{
+				mineMap->GetWindow().close();
+				return;
+			}
+		}
+	}
+
+	void CheckWin()
+	{
+		for (int y = 0; y < map_size_y; y++)
+		{
+			for (int x = 0; x < map_size_x; x++)
+			{
+				if (!(map[y][x] == map_view[y][x] || (map[y][x] == -1 && map_view[y][x] == 9) || (map[y][x] == 0 && map_view[y][x] == 10)))
+				{
+					return;
+				}
+			}
+		}
+		std::string t = "You won!";
+		GameOver(t);
+		return;
+	}
+
+	void Loop()
+	{
+		sf::Event e;
+		while (mineMap->GetWindow().isOpen())
+		{
+			while (mineMap->GetWindow().pollEvent(e))
+			{
+				if (e.type == sf::Event::Closed)
+				{
+					mineMap->GetWindow().close();
+				}
+
+				if (e.type == sf::Event::MouseButtonPressed)
+				{
+
+					int x = (int)(e.mouseButton.x / square_size);
+					int y = (int)(e.mouseButton.y / square_size);
+
+					if (x < map_size_x && x >= 0 && y < map_size_y && y >= 0)
+					{
+						if (e.mouseButton.button == sf::Mouse::Right)
+						{
+							if (map_view[y][x] != 9 && map_view[y][x] == 10) // 9 - flag
+							{
+								map_view[y][x] = 9;
+							}
+							else if (map_view[y][x] == 9)
+							{
+								map_view[y][x] = 10;
+							}
+						}
+
+						if (e.mouseButton.button == sf::Mouse::Left)
+						{
+							map_view[y][x] = map[y][x];
+							if (map[y][x] == 0)
+							{
+								mineMap->OpenAllZeroAround(x, y);
+							}
+							if (map[y][x] == -1)
+							{
+								std::string t = "Game over!";
+								mineMap->ShowMine(x, y);
+								GameOver(t);
+								//return;
+							}
+							CheckWin();
+						}
+					}
+				}
+			}
+
+			//draw
+			if (!isGameOver) {
+				mineMap->GetWindow().clear(sf::Color::Black);
+				for (int y = 0; y < map_size_y; y++)
+				{
+					for (int x = 0; x < map_size_x; x++)
+					{
+						mineMap->SetSquareOnScreen(x, y);
+						if (map_view[y][x] == 10)
+						{
+							mineMap->ShowSquareSprite(x, y);
+						}
+
+						if (map_view[y][x] == -1)
+						{ //mine
+							mineMap->ShowMine(x, y);
+						}
+						else if (map_view[y][x] == 9)
+						{
+							mineMap->ShowFlagONScreen(x, y);
+						}
+
+						else if (map_view[y][x] != 10)
+						{
+							mineMap->ShowColorText(x, y);
+						}
+					}
+				}
+			}
+			
+			mineMap->GetWindow().display();
+		}
+	}
+
+	void GenerateMineMap() {
+		mineMap->GenerateMap();
+	}
+};
 int main()
 {
 	//import Textures and font
+	std::unique_ptr<Map> map = std::make_unique<Map>();
+	std::unique_ptr<GameManager> manager = std::make_unique<GameManager>(map.get());
 
-	flag.loadFromFile("Textures/flag.png");
-	mine.loadFromFile("Textures/mine.png");
-	square.loadFromFile("Textures/Square.png");
-
-	font.loadFromFile("Textures/FutureLight-BW15w.ttf");
-
-	GenerateMap();
-	Loop();
+	manager->GenerateMineMap();
+	manager->Loop();
 }
